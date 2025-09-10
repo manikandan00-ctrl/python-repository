bb69f3dc018e670ba0feaa8c1b05ac4061333fe7
manikandan00-ctrl <rm1000409@gmail.com>
2025-09-10 18:02:09 +0530

    Add files via upload

---
+++ github2.py
@@ -0,0 +1,11 @@
+def biggest():
+    a=int(input("enter a number"))
+    b=int(input("enter a number"))
+    c=int(input("enter a number"))
+    if a>b and a>c:
+        print("a is grester")
+    elif b>c and b>a:
+         print("b is greater")
+    else:
+         print("c is greater")
+biggest()
